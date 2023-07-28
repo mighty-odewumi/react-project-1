2c0e7b334ccd69a42f205ceb10ee76bbc48b57db
gr1ntch <odewumimighty@gmail.com>
2023-07-28 03:00:15 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import Main from "./components/Main";
+import NavBar from "./components/NavBar";
+
+export default function App() {
+  return (
+    <div className="container">
+      <NavBar />
+      <Main />
+    </div>
+  )
+}
---
+++ components/NavBar.js
@@ -0,0 +1,9 @@
+export default function NavBar() {
+  return (
+      <nav>
+          <img src="./logo192.png" alt="React library logo" className="logo-img"/>
+          <h3>ReactFacts</h3>
+          <h4>React Course - Project 1</h4>
+      </nav>
+  )
+}
